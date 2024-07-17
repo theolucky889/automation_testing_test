3f6700747f9e367971b2d41c033a328a13d78d8a
theolucky889 <theolucky889@gmail.com>
2024-07-18 04:47:29 +0800

    Update selenium_test.py

--- selenium_test.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from time import sleep
-
-options = Options()
-
-options.add.experimental_option("excludeSwitches", ["enable-logging"])
-driver = webdriver.Chrome(options=options)
