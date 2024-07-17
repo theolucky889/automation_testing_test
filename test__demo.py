2d66807adc18936357815ee91ec97c4ec6826669
theolucky889 <theolucky889@gmail.com>
2024-07-18 05:23:14 +0800

    Update test__demo.py

--- test__demo.py
+++ test__demo.py
@@ -17,3 +17,13 @@ driver.find_element(By.ID, "user-name").send_keys("standard_user")
 driver.find_element(By.ID, "password").send_keys("secret_sauce")
 driver.find_element(By.ID, ("login-button").click())
 sleep(5)
+
+# test if login success
+text = driver.find_element(By.CLASS_NAME, "title").text
+
+assert "products" in text.lower()
+
+print("TEST PASSED : LOGGIN SUCCESSFULL")
+
+# close the driver
+driver.quit()
