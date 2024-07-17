7655fc29196432686b389e6ee50b61c53794f8b2
theolucky889 <theolucky889@gmail.com>
2024-07-18 05:24:27 +0800

    Update test__demo.py

--- test__demo.py
+++ test__demo.py
@@ -10,12 +10,12 @@ options.add_experimental_option("excludeSwitches", ["enable-logging"])
 print("testing started")
 driver = webdriver.Chrome(options=options)
 
-driver.get("https://wwww.saucedemo.com/")
+driver.get("https://www.saucedemo.com/")
 sleep(3)
 
 driver.find_element(By.ID, "user-name").send_keys("standard_user")
 driver.find_element(By.ID, "password").send_keys("secret_sauce")
-driver.find_element(By.ID, ("login-button").click())
+driver.find_element(By.ID, "login-button").click()
 sleep(5)
 
 # test if login success
@@ -23,7 +23,7 @@ text = driver.find_element(By.CLASS_NAME, "title").text
 
 assert "products" in text.lower()
 
-print("TEST PASSED : LOGGIN SUCCESSFULL")
+print("TEST PASSED : LOGIN SUCCESSFULL")
 
 # close the driver
 driver.quit()
