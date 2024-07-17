c2b3dde873b5327cde587a4f3a1d358e2bf60e53
theolucky889 <theolucky889@gmail.com>
2024-07-18 05:21:14 +0800

    Update test__demo.py

--- test__demo.py
+++ test__demo.py
@@ -3,3 +3,17 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from time import sleep
 
+# set options for not prompting devtools info
+options = Options()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+print("testing started")
+driver = webdriver.Chrome(options=options)
+
+driver.get("https://wwww.saucedemo.com/")
+sleep(3)
+
+driver.find_element(By.ID, "user-name").send_keys("standard_user")
+driver.find_element(By.ID, "password").send_keys("secret_sauce")
+driver.find_element(By.ID, ("login-button").click())
+sleep(5)
